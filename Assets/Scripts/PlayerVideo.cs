929c79f8f74b48f25aa852b5e52400ef6ca9ebd2
ArsLudumAcrux <backupthecurehunt@gmail.com>
2018-11-30 17:34:00 -0200

    avanço na cutscene
    agafgafgafgar

---
+++ Assets/Scripts/PlayerVideo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayerVideo : MonoBehaviour {
+
+    Animation cutScene;
+
+    private void Awake()
+    {
+        cutScene = GetComponent<Animation>();
+    }
+    public void _Play()
+    {
+        cutScene.Play();
+    }
+    public void _Stop()
+    {
+        cutScene.Stop();
+    }
+    public void Button()
+    {
+        _Play();
+    }
+}
