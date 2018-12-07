ab4fe3d0cb903abe1c1417e33e191549cd39e7ff
ArsLudumAcrux <backupthecurehunt@gmail.com>
2018-12-07 17:26:52 -0200

    cutscene 07-12
    cutscene feita no Premier

--- Assets/Scripts/CutScene.cs
+++ Assets/Scripts/CutScene.cs
@@ -6,16 +6,21 @@ public class CutScene : MonoBehaviour {
 
     public Animation anim;
 
+    public void Awake()
+    {
+        anim=GetComponent<Animation>();
+    }
+
     public void PlayAnimation()
     {
-        anim.Play();
-        //Time.timeScale = 1f;
+        //anim.Play("Cutscene Test 2");
+        Time.timeScale = 1f;
         Debug.Log("Time scale = 1");
     }
     public void StopAnimation()
     {
-        anim.Stop();
-        //Time.timeScale = 0f;
+        //anim.Stop();
+        Time.timeScale = 0f;
         Debug.Log("Time scale = 0");
     }
 }
