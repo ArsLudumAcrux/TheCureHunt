21f55e817bbe03d84f28f1c500256f03f4d1abdc
ArsLudumAcrux <43455385+ArsLudumAcrux@users.noreply.github.com>
2018-12-07 00:02:39 -0200

    cutscene quase la
    aedss

---
+++ Assets/Scripts/CutScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutScene : MonoBehaviour {
+
+    public Animation anim;
+
+    public void PlayAnimation()
+    {
+        anim.Play();
+        //Time.timeScale = 1f;
+        Debug.Log("Time scale = 1");
+    }
+    public void StopAnimation()
+    {
+        anim.Stop();
+        //Time.timeScale = 0f;
+        Debug.Log("Time scale = 0");
+    }
+}
